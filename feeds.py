9bb8c2f95869d5228fcba28892aa53011f3f7f02
Nathan Bergey <nathan.bergey@gmail.com>
2013-09-07 16:12:08 -0700

    lots of documentation in message reader class

--- feeds.py
+++ feeds.py
@@ -43,32 +43,59 @@ FEEDS = {
 }
 
 class MessageReader(object):
+    """A message reader class for PASA FC messages"""
 
-    message_header = struct.Struct('!4sHLH')
+    # This header is consistant across messages
+    header = struct.Struct('!4sHLH')
 
     def __init__(self, messages):
         self.messages = messages
 
     def decode_packet(self, packet):
+        """A decoder for a packet"""
 
         # Read header
-        s = self.message_header
-        fourcc, timestamp_hi, timestamp_lo, length = s.unpack(packet[:s.size])
-        timestamp = timestamp_hi << 32 | timestamp_lo
-        packet = packet[s.size:]
-        print fourcc, timestamp, length
+        fourcc, timestamp_hi, timestamp_lo, message_length = self.header.unpack(packet[:self.header.size])
 
-        # read body
+        # fix timestamp
+        timestamp = timestamp_hi << 32 | timestamp_lo
+
+        # truncate what we've already read
+        packet = packet[self.header.size:]
+        print fourcc, timestamp, message_length
+
+        # Read body
+        # get message type from header
         message_type = self.messages.get(fourcc, None)
         if message_type is not None:
+
+            # init a container for the values
             body = {'type': fourcc}
+            message_size = 0
+
+            # a temporary container for the body with a guess at the orginal lenght:
+            body_packet = packet[:message_length]
+
+            # Try to read through the fields as specified in the config
             for field in message_type.get('members', []):
                 s = field['struct']
-                body[field['key']], = s.unpack(packet[:s.size])
+
+                try:
+                    body[field['key']], = s.unpack(body_packet[:s.size])
+                    message_size += s.size
+                except:
+                    #TODO: handle this case
+                    pass
+ 
                 # truncate what we already read
-                packet = packet[s.size:]
+                body_packet = body_packet[s.size:]
+
+            # check to see if we read the right number of bytes
+            if message_length != message_size:
+                print "Ahh!", message_length, message_size
             print body
 
+# list of message types used in the message_type key
 MESSAGE_TYPES = {
     'messages': MessageReader,
 }
