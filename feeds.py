7847fd73fb48f07025b4651635154947f977a0de
Nathan Bergey <nathan.bergey@gmail.com>
2013-09-08 16:26:29 -0700

    precompute struct info in message class

--- feeds.py
+++ feeds.py
@@ -13,25 +13,27 @@ fc = {
     'message_type': "messages",
     'messages': {
         'ADIS': {
+            'endianness': '<',
             'members': [
-                {'key': "VCC",     'struct': struct.Struct("<h"), 'units': {'mks': "volt", 'scale': 2.418}},
-                {'key': "Gryo_X",  'struct': struct.Struct("<h"), 'untis': {'mks': "hertz", 'scale': 0.05}},
-                {'key': "Gryo_Y",  'struct': struct.Struct("<h"), 'units': {'mks': "hertz", 'scale': 0.05}},
-                {'key': "Gryo_Z",  'struct': struct.Struct("<h"), 'units': {'mks': "hertz", 'scale': 0.05}},
-                {'key': "Acc_X",   'struct': struct.Struct("<h"), 'units': {'mks': "meter/s/s", 'scale': 3.33}},
-                {'key': "Acc_Y",   'struct': struct.Struct("<h"), 'units': {'mks': "meter/s/s", 'scale': 3.33}},
-                {'key': "Acc_Z",   'struct': struct.Struct("<h"), 'units': {'mks': "meter/s/s", 'scale': 3.33}},
-                {'key': "Magn_X",  'struct': struct.Struct("<h"), 'units': {'mks': "tesla", 'scale': 0.5}},
-                {'key': "Magn_X",  'struct': struct.Struct("<h"), 'units': {'mks': "tesla", 'scale': 0.5}},
-                {'key': "Magn_X",  'struct': struct.Struct("<h"), 'units': {'mks': "tesla", 'scale': 0.5}},
-                {'key': "Temp",    'struct': struct.Struct("<h"), 'units': {'mks': "kelvin", 'scale': 0.14}},
-                {'key': "Aux_ADC", 'struct': struct.Struct("<h"), 'units': {'mks': "volt", 'scale': 806}},
+                {'key': "VCC",     'struct': "h", 'units': {'mks': "volt", 'scale': 2.418}},
+                {'key': "Gryo_X",  'struct': "h", 'untis': {'mks': "hertz", 'scale': 0.05}},
+                {'key': "Gryo_Y",  'struct': "h", 'units': {'mks': "hertz", 'scale': 0.05}},
+                {'key': "Gryo_Z",  'struct': "h", 'units': {'mks': "hertz", 'scale': 0.05}},
+                {'key': "Acc_X",   'struct': "h", 'units': {'mks': "meter/s/s", 'scale': 3.33}},
+                {'key': "Acc_Y",   'struct': "h", 'units': {'mks': "meter/s/s", 'scale': 3.33}},
+                {'key': "Acc_Z",   'struct': "h", 'units': {'mks': "meter/s/s", 'scale': 3.33}},
+                {'key': "Magn_X",  'struct': "h", 'units': {'mks': "tesla", 'scale': 0.5}},
+                {'key': "Magn_X",  'struct': "h", 'units': {'mks': "tesla", 'scale': 0.5}},
+                {'key': "Magn_X",  'struct': "h", 'units': {'mks': "tesla", 'scale': 0.5}},
+                {'key': "Temp",    'struct': "h", 'units': {'mks': "kelvin", 'scale': 0.14}},
+                {'key': "Aux_ADC", 'struct': "h", 'units': {'mks': "volt", 'scale': 806}},
             ],
         },
         'ROLL': {
+            'endianness': '<',
             'members': [
-                {'key': "PWM", 'struct': struct.Struct("H"), 'units': {'mks': "seconds", 'scale': 0}},
-                {'key': "Disable", 'struct': struct.Struct("B")},
+                {'key': "PWM", 'struct': "H", 'units': {'mks': "seconds", 'scale': 0}},
+                {'key': "Disable", 'struct': "B"},
             ],
         },
     },
@@ -42,6 +44,7 @@ FEEDS = {
     'fc': fc,
 }
 
+
 class MessageReader(object):
     """A message reader class for PASA FC messages"""
 
@@ -50,6 +53,16 @@ class MessageReader(object):
 
     def __init__(self, messages):
         self.messages = messages
+
+        # pre compute steps
+        self.compute_sizes()
+
+    def compute_sizes(self):
+        for message in self.messages:
+            struct_string = self.messages[message]['endianness']
+            for member in self.messages[message]['members']:
+                struct_string += member['struct']
+            self.messages[message]['struct'] = struct.Struct(struct_string)
 
     def decode_packet(self, packet):
         """A decoder for a packet"""
@@ -71,28 +84,19 @@ class MessageReader(object):
 
             # init a container for the values
             body = {'type': fourcc}
-            message_size = 0
-
-            # a temporary container for the body with a guess at the orginal lenght:
-            body_packet = packet[:message_length]
-
-            # Try to read through the fields as specified in the config
-            for field in message_type.get('members', []):
-                s = field['struct']
-
-                try:
-                    body[field['key']], = s.unpack(body_packet[:s.size])
-                    message_size += s.size
-                except:
-                    #TODO: handle this case
-                    pass
- 
-                # truncate what we already read
-                body_packet = body_packet[s.size:]
 
             # check to see if we read the right number of bytes
-            if message_length != message_size:
-                print "Ahh!", message_length, message_size
+            if message_length != message_type['struct'].size:
+                # If the message isn't the right length, lets try and unpack it
+                # using the size of the struct.
+                message_length = message_type['struct'].size
+
+            unpacked = message_type['struct'].unpack(packet[:message_length])
+            for i, field in enumerate(message_type['members']):
+                body[field['key']] = unpacked[i] * field.get('units', {}).get('scale', 1)
+
+            # truncate what we've already read
+            packet = packet[message_length:]
             print body
 
 # list of message types used in the message_type key
