1f3f82c2bd1b56da4f092c5df2fee34422587b12
Nathan Bergey <nathan.bergey@gmail.com>
2013-10-26 13:51:39 -0700

    a little clean up in text widget

--- static/widgets/text_widget.js
+++ static/widgets/text_widget.js
@@ -1,26 +1,26 @@
 function TextWidget(config) {
-    function pad(n, width, z) {
-      z = z || '0';
-      n = n + '';
-      return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-    }
+
     Widget.call(this, config);
 
-    //$('.container').append("<script type=\"text/javascript\">document.getElementById('widget " + this.config.id + "').style.width=\"" + this.config.width + "px\";</script>");
-    //$('.container').append("<script type=\"text/javascript\">document.getElementById('widget " + this.config.id + "').style.height=\"" + this.config.height + "px\";</script>");
+    function pad(n, width, z) {
+        z = z || '0';   // Defualt padding char
+        n = n + '';
+        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    }
+
 
     var text = '<h2>' + config.id + '</h2>';
     text    += '<table><tbody>';
-    this.config.controls.forEach(function(control) {
+    this.config.controls.forEach( function (control) {
        text += '<tr><td class="label">' + control.label + ':' + '</td><td> </td><td> </td></tr>';
     });
     text    += '</tbody></table>';
     this.div.innerHTML = text;
-    
-    this.onControlChanged = function(control) {
+
+    this.onControlChanged = function (control) {
         var text = '<h2>' + config.id + '</h2>';
         text    += '<table><tbody>';
-        this.config.controls.forEach(function(control) {
+        this.config.controls.forEach( function (control) {
            text += '<tr><td class="label">';
            text += control.label;
            text += ':</td><td><value style="color:';
@@ -35,7 +35,7 @@ function TextWidget(config) {
         this.setText(text);
     };
 
-    this.setText = function(text) {
+    this.setText = function (text) {
         this.div.innerHTML = text;
     };
 }
