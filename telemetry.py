4b1e9ab9a052d4e269d34ae12568158eb2b626cc
Nathan Bergey <nathan.bergey@gmail.com>
2013-11-14 22:14:36 -0800

    not initilizing any sockets if not calling telemtry from __main__

--- telemetry.py
+++ telemetry.py
@@ -4,35 +4,40 @@
 import config
 import server
 from feeds import FEEDS
-import threading
 import Queue
 
 # Make a Queue
 q = Queue.Queue()
 
-# Init tornado
-web = server.Webservice(q)
+def run():
 
-# Spin up listener threads
-threads = []
-for key, feed in FEEDS.iteritems():
-    ip = feed['ip']
-    port = feed['port']
-    reader = feed['message_type'](feed['messages'])
-    listener = feed['listener'](ip, port, reader)
-    listener.add_queue(q)
-    threads.append(listener)
+    # Spin up listener threads
+    threads = []
+    for key, feed in FEEDS.iteritems():
+        ip = feed['ip']
+        port = feed['port']
+        reader = feed['message_type'](feed['messages'])
+        listener = feed['listener'](ip, port, reader)
+        listener.add_queue(q)
+        threads.append(listener)
 
 
-# Start
-try:
-    for thread in threads:
-        thread.start()
 
-    # run tornado in main thread
-    web.run()
+    try:
+        # Init tornado
+        web = server.Webservice(q)
 
-except KeyboardInterrupt, SystemExit:
-    for thread in threads:
-        thread.stop()
 
+        for thread in threads:
+            thread.start()
+
+        # run tornado in main thread
+        web.run()
+
+    except KeyboardInterrupt, SystemExit:
+        for thread in threads:
+            thread.stop()
+
+
+if __name__ == '__main__':
+    run()
